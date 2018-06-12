b230f0faea451afb4e83a9befd25afd960931393
hezzrrah <hezzrrah@gmail.com>
2018-06-12 23:48:19 +0300

    midleware

--- server.js
+++ server.js
@@ -1,28 +1,48 @@
 const express = require('express');
 const hbs = require('hbs');
-
+const fs = require('fs');
 var app = express();
 
+hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
 
+app.use((req, res, next) => {
+    var now = new Date().toString();
+    var log = `${now}: ${req.method}: ${req.ip}: ${req.url}`;
+    console.log(log);
+    fs.appendFile('app.log', log + '\n', (err) => {
+        if (err) {
+            console.log("General failure in logging module.");
+        }
+    })
+
+    next();
+});
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+})
+
+hbs.registerHelper('screemIt', (text) => {
+    return text.toUpperCase();
+})
 
 app.get('/', (req, res) => {
-
     //res.send('<h1>Hello, you express!</h1>');
-    res.send({
-        name: 'cats',
-        color: 'green',
-        eats: [
-            'fish',
-            'cans',
-            'things'
-        ]
-    });
+    res.render('home.hbs', {
+        pageTitle: 'Home page',
+        heading: 'Home page',
+        welcomeMessage: 'Welcome to my lorem ipsum domain, here i am lorem and you are ipsum, so watch it !',
+    })
 })
 
 app.get('/about', (req, res) => {
-    res.render('about.hbs')
+    res.render('about.hbs', {
+        pageTitle: 'Some title',
+        heading: 'About all the living things and the mouses',
+        h_txt: 'This is a new way to present my cooking manners - by stairing at you and thinking, thinking...',
+    })
 });
 
 app.get('/profile', (req, res) => {
